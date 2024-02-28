944e885097a370dd939289b581e1b18ff2b4a676
GaurangGatlewar <gvg228@nyu.edu>
2024-02-27 21:19:37 -0500

    Leetcode Kids with Greatest candies alternate solution

--- KidsWithGreatestCandies.py
+++ KidsWithGreatestCandies.py
@@ -1,4 +1,4 @@
 class KidsWithGreatestCandies:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        limit = max(candies)-extraCandies
-        return [candy>=limit for candy in candies]
+        m = max(candies) - extraCandies
+        return map(lambda x: x>=m, candies)
