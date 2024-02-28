042b8c5f15020713bdd8c252cc14b80698ca774d
GaurangGatlewar <gvg228@nyu.edu>
2024-02-28 02:46:03 -0500

    Leetcode Increasing triplet subsequence optimized solution

--- IncreasingTriplet.py
+++ IncreasingTriplet.py
@@ -1,13 +1,11 @@
 class IncreasingTriplet:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        minTable = [nums[0]]
-        for i in range(1, len(nums)):
-            minTable.append(min(minTable[-1],nums[i]))
-        maxTable = [nums[-1]]
-        for i in range(len(nums)-2, -1, -1):
-            maxTable.append(max(maxTable[-1],nums[i]))
-        maxTable = maxTable[::-1]
-        for i in range(1, len(nums)-1):
-            if (minTable[i-1]<nums[i]) and (nums[i]<maxTable[i+1]):
+        smallest = smaller = float(inf)
+        for num in nums:
+            if num <= smallest:
+                smallest = num
+            elif num <= smaller:
+                smaller = num
+            else:
                 return True
         return False
