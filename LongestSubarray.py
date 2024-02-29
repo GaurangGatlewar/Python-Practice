5028980febabe14228b8519adb82e97710e78a05
GaurangGatlewar <gvg228@nyu.edu>
2024-02-29 03:45:12 -0500

    Leetcode Longest subarray of 1's after deleting one element solution

--- LongestSubarray.py
+++ LongestSubarray.py
@@ -1,21 +1,18 @@
-class MaxConsecutiveOnesIII:
-    def longestOnes(self, nums: List[int], k: int) -> int:
-        if sum(nums)+k>=len(nums):
-            return len(nums)
-
+class LongestSubarray:
+    def longestSubarray(self, nums: List[int]) -> int:
+        if len(nums) == sum(nums): return len(nums)-1
         start = end = 0
         counter = 0
         maxOnes = 0
         total = 0
         while end < len(nums):
-            if ((counter < k) or (nums[end]==1)):
+            if ((counter == 0) or (nums[end]==1)):
+                total += nums[end]
                 counter += (1-nums[end])
                 end += 1
-                total += 1
             else:
+                total -= nums[start]
                 counter -= (1-nums[start])
                 start += 1
-                total -= 1
             if total > maxOnes: maxOnes = total
-
         return maxOnes
